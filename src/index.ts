dced65b413ad076c073f1d521ab7a9f5f88067d8
Opia12345 <preciousopia7@gmail.com>
2025-10-25 15:40:34 +0100

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   // "http://localhost:3000",
   "https://caring-sparks.vercel.app",
-  "https://theprgod.com/"
+  "https://theprgod.com"
 ];
 
 app.use(
